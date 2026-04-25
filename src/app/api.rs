0341dc3ff22e5cd04c53ec16f34efdaa1c622caf
Rishikesh Dash <rishikeshdash03@gmail.com>
2026-04-26 02:58:05 +0530

    error handling in get and set requests

--- src/app/api.rs
+++ src/app/api.rs
@@ -1,13 +1,17 @@
 use axum::{
     Json,
-    extract::{Query, State},
-    http,
-    response::{Html, IntoResponse},
+    extract::State,
+    response::{Html, Redirect},
 };
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use url::Url;
 
-use crate::app::{AppState, model::ShortUrl, utils};
+use crate::app::{
+    AppState,
+    error::{AppError, AppQuery},
+    model::ShortUrl,
+    utils,
+};
 
 pub async fn get_index() -> Html<String> {
     match tokio::fs::read_to_string("static/index.html").await {
@@ -21,56 +25,34 @@ pub struct UrlRequest {
     url: Url,
 }
 
-#[derive(Serialize, Debug)]
-pub enum UrlResponse {
-    Message(String),
-    ShortUrl(ShortUrl),
-}
-
-impl IntoResponse for UrlResponse {
-    fn into_response(self) -> axum::response::Response {
-        let status_code = match self {
-            UrlResponse::Message(_) => http::StatusCode::NOT_FOUND,
-            UrlResponse::ShortUrl(_) => http::StatusCode::OK,
-        };
-        (status_code, Json(self)).into_response()
-    }
-}
-
 #[axum::debug_handler]
 pub async fn get_short_url(
     State(state): State<AppState>,
-    Query(payload): Query<UrlRequest>,
-) -> UrlResponse {
+    AppQuery(payload): AppQuery<UrlRequest>,
+) -> Result<Redirect, AppError> {
     let short_code = payload
         .url
         .as_str()
         .replace(state.config.get_url().as_str(), "");
-    // println!("{}", short_code);
 
     match ShortUrl::get_url_from_db(&state.db, &short_code).await {
-        Ok(Some(val)) => UrlResponse::ShortUrl(val),
-        Ok(None) => UrlResponse::Message("Url not found".to_string()),
-        Err(_) => UrlResponse::Message("Database Error".to_string()),
+        Ok(Some(val)) => Ok(Redirect::temporary(val.long_url.as_str())),
+        Ok(None) => Err(AppError::URLNotFound),
+        Err(e) => Err(e),
     }
 }
 
+#[axum::debug_handler]
 pub async fn set_short_url(
     State(state): State<AppState>,
     Json(payload): Json<UrlRequest>,
-) -> UrlResponse {
+) -> Result<Json<ShortUrl>, AppError> {
     let short_code = &utils::generate_hash(payload.url.as_str())[..7];
     match ShortUrl::get_url_from_db(&state.db, short_code).await {
-        Ok(Some(val)) => {
-            // println!("Value present");
-            UrlResponse::ShortUrl(val)
-        }
+        Ok(Some(val)) => Ok(Json(val)),
         Ok(None) => {
-            let Ok(short_url_map) = ShortUrl::build(&state.db, &state.config, payload.url).await
-            else {
-                return UrlResponse::Message("Database Error".to_string());
-            };
-            let Ok(_) = sqlx::query!(
+            let short_url_map = ShortUrl::build(&state.db, &state.config, payload.url).await?;
+            let _ = sqlx::query!(
                 r#"
                     INSERT INTO short_urls (long_url, short_url, short_code)
                     VALUES ($1, $2, $3);
@@ -80,13 +62,10 @@ pub async fn set_short_url(
                 short_url_map.short_code
             )
             .execute(&state.db)
-            .await
-            else {
-                return UrlResponse::Message("Database Error".to_string());
-            };
+            .await?;
 
-            UrlResponse::ShortUrl(short_url_map)
+            Ok(Json(short_url_map))
         }
-        Err(_) => UrlResponse::Message("Database Error".to_string()),
+        Err(e) => Err(e),
     }
 }
--- src/app/error.rs
+++ src/app/error.rs
@@ -1,8 +1,44 @@
+use std::sync::Arc;
+
+use axum::{
+    Json,
+    extract::{
+        FromRequest, FromRequestParts, Query,
+        rejection::{JsonRejection, QueryRejection},
+    },
+    http::StatusCode,
+    response::IntoResponse,
+};
+use serde::Serialize;
+
 #[derive(Debug)]
 pub enum AppError {
     DataBaseError(sqlx::Error),
+    JsonRejection(JsonRejection),
+    QueryRejection(QueryRejection),
+    URLNotFound,
     HashError,
     ParseError(url::ParseError),
+}
+
+#[derive(FromRequest)]
+#[from_request(via(axum::Json), rejection(AppError))]
+pub struct AppJson<T>(pub T);
+
+#[derive(FromRequestParts)]
+#[from_request(via(Query), rejection(AppError))]
+pub struct AppQuery<T>(pub T);
+
+impl From<JsonRejection> for AppError {
+    fn from(err: JsonRejection) -> Self {
+        AppError::JsonRejection(err)
+    }
+}
+
+impl From<QueryRejection> for AppError {
+    fn from(err: QueryRejection) -> Self {
+        AppError::QueryRejection(err)
+    }
 }
 
 impl From<sqlx::Error> for AppError {
@@ -14,5 +50,43 @@ impl From<sqlx::Error> for AppError {
 impl From<url::ParseError> for AppError {
     fn from(err: url::ParseError) -> Self {
         AppError::ParseError(err)
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        #[derive(Serialize)]
+        struct ErrorResponse {
+            message: String,
+        }
+
+        let (status, message, err) = match &self {
+            AppError::JsonRejection(rejection) => (rejection.status(), rejection.body_text(), None),
+            AppError::QueryRejection(rejection) => {
+                (rejection.status(), rejection.body_text(), None)
+            }
+            AppError::DataBaseError(_e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Database error".to_owned(),
+                Some(self),
+            ),
+
+            AppError::HashError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Hashing error".to_owned(),
+                Some(self),
+            ),
+            AppError::ParseError(_e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Parsing error".to_owned(),
+                None,
+            ),
+            AppError::URLNotFound => (StatusCode::NOT_FOUND, "URL not found".to_owned(), None),
+        };
+        let mut response = (status, Json(ErrorResponse { message })).into_response();
+        if let Some(err) = err {
+            response.extensions_mut().insert(Arc::new(err));
+        }
+        response
     }
 }
